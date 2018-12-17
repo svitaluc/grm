ba3f93cfe6c2e0dd3fb3866f4376a30dc583aafe
svitaluc <svitaluc@gmail.com>
2018-12-17 21:38:13 +0100

    more PA runs

--- src/main/java/helpers/DefaultClusterMapper.java
+++ src/main/java/helpers/DefaultClusterMapper.java
@@ -1,14 +1,25 @@
 package helpers;
 
+import org.janusgraph.graphdb.idmanagement.IDManager;
+
 public class DefaultClusterMapper implements ClusterMapper {
     private int numClusters;
+    private IDManager idManager;
 
     @Override
     public long map(long vertexId) {
-        return Math.abs(Long.hashCode(vertexId) % numClusters);
+        if(idManager == null)
+            return Math.abs(Long.hashCode(vertexId) % numClusters);
+        return Math.abs(Long.hashCode(idManager.fromVertexId(vertexId)) % numClusters);
+//        return Math.abs(vertexId % numClusters);
     }
 
     public DefaultClusterMapper(int numClusters) {
         this.numClusters = numClusters;
+    }
+
+    public DefaultClusterMapper(int numClusters, IDManager idManager) {
+        this.numClusters = numClusters;
+        this.idManager = idManager;
     }
 }
